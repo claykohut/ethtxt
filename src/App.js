de38b1d7efb8040e756a12fc2e450c843d069746
Clay Kohut <claykohut@MacBook-Pro-3.local>
2018-02-18 00:02:25 -0500

    got stuff working with read

--- src/App.js
+++ src/App.js
@@ -50,21 +50,38 @@ class App extends Component {
     // Declaring this for later so we can chain functions on SimpleStorage.
     var simpleStorageInstance
 
-    // Get accounts.
+    // this.state.web3.eth.getAccounts((error, accounts) => {
+    //   simpleStorage.deployed().then((instance) => {
+    //     simpleStorageInstance = instance
+    //   //
+    //     // Stores a given value, 5 by default.
+    //     return simpleStorageInstance.set(15, {from: accounts[0]})
+    //   }).then((result) => {
+    //     // Get the value from the contract to prove it worked.
+    //     console.log('value is?? ', simpleStorageInstance.get.call(accounts[0]))
+    //     return simpleStorageInstance.get.call(accounts[0])
+    //   })
+    //   .then((result) => {
+    //     // Update state with the result.
+    //     console.log('got result? ', result)
+    //     return this.setState({ storageValue: result.c[0] })
+    //   })
+    // })
+
     this.state.web3.eth.getAccounts((error, accounts) => {
       simpleStorage.deployed().then((instance) => {
         simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
         // Get the value from the contract to prove it worked.
+        console.log('value is?? ', simpleStorageInstance.get.call(accounts[0]))
         return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
+      })
+      .then((result) => {
         // Update state with the result.
+        console.log('got result? ', result)
         return this.setState({ storageValue: result.c[0] })
       })
     })
+
   }
 
   render() {
