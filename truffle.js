0d7041d425a40c249e3ff948123d1be0f015fd9c
Clay Kohut <claykohut@MacBook-Pro-3.local>
2018-02-19 19:07:02 -0500

    prevent duplicate keys

--- truffle.js
+++ truffle.js
@@ -5,13 +5,15 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 9545,
+      gasPrice: 10000000000, // 10 GWEI
       network_id: "*" // Match any network id
     },
     ropsten:  {
        network_id: 3,
        host: "localhost",
        port:  8545,
-       gas:   2900000
+       gas:   2900000,
+       gasPrice: 10000000000 // 10 GWEI
     }
   }
 };
