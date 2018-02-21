868ccc934ea278c35fc1045c4e69b4dcb7449fd8
Clay Kohut <claykohut@MacBook-Pro-3.local>
2018-02-20 22:43:02 -0500

    use livenet api

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-export const ETH_PROVIDER_URL = 'http://127.0.0.1:9545';
+export const ETH_PROVIDER_URL = 'https://api.myetherapi.com/eth'; //'http://127.0.0.1:9545';
 
 export const ETH_PROVIDER_URL_ROPSTEN = 'https://api.myetherapi.com/rop';
