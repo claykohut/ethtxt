ca7bddfcde32a23882820850099ed1b723245527
Clay Kohut <claykohut@MacBook-Pro-3.local>
2018-03-03 12:02:20 -0500

    moved index back

---
+++ src/reducers/archivedText/index.js
@@ -0,0 +1,29 @@
+import {
+  SET_FEATURED_TEXT,
+  SUBMIT_TEXT_START,
+  SUBMIT_TEXT_END,
+} from './actions';
+
+const initialState = {
+  text: '',
+  submitting: false,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_FEATURED_TEXT:
+      return {
+        text: action.payload.text,
+      };
+    case SUBMIT_TEXT_START:
+      return {
+        submitting: true,
+      };
+    case SUBMIT_TEXT_END:
+      return {
+        submitting: false,
+      };
+    default:
+      return state;
+  }
+};
